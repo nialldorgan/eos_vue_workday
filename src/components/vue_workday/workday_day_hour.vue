d307ba51c34048e9268d1bea39dada0eecd67af2
nialldorgan <38695538+nialldorgan@users.noreply.github.com>
2022-06-28 16:55:10 +0100

    bug fix workdays with shorter hours not displaying with shortened day

--- src/components/vue_workday/workday_day_hour.vue
+++ src/components/vue_workday/workday_day_hour.vue
@@ -14,7 +14,11 @@
 <script>
 /*  global  */
 import dayjs from 'dayjs'
+import isSameOrBefore from 'dayjs/plugin/isSameOrBefore'
+import isSameOrAfter from 'dayjs/plugin/isSameOrAfter'
 import WorkdayCell from './workday_cell.vue'
+dayjs.extend(isSameOrBefore)
+dayjs.extend(isSameOrAfter)
 export default {
   name: 'WorkdayDayHour',
 
@@ -48,14 +52,17 @@ export default {
       let startHour = dayjs(`${dayjs(day.value).format('YYYY-MM-DD')} ${dayjs(start).format('HH:mm')}`)
       let end = startHour.add(1, 'hour')
       let hourCells = []
-      while(startHour < end) {
-        hourCells.push({
-          value: startHour.format('YYYY-MM-DD HH:mm'),
-          index: index
-        })
-        // five minute intervals
-        startHour = startHour.add(5, 'minutes')
-        index++
+      if (dayjs(this.workday.start_time).isSameOrBefore(startHour) && dayjs(this.workday.finish_time).isSameOrAfter(end))
+      {        
+        while(startHour < end) {
+          hourCells.push({
+            value: startHour.format('YYYY-MM-DD HH:mm'),
+            index: index
+          })
+          // five minute intervals
+          startHour = startHour.add(5, 'minutes')
+          index++
+        }
       }
       return hourCells
     },
